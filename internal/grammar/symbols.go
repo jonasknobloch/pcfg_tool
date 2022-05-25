b13a418c59b410c49e5b5c93caacf35778c9be40
Jonas Knobloch <jonas.knobloch@t-online.de>
2022-05-25 12:55:50 +0200

    Add mutex to symbol table

--- internal/grammar/symbols.go
+++ internal/grammar/symbols.go
@@ -1,11 +1,15 @@
 package grammar
 
-import "errors"
+import (
+	"errors"
+	"sync"
+)
 
 type SymbolTable struct {
 	index int
 	atoi  map[string]int
 	itoa  map[int]string
+	mutex sync.RWMutex
 }
 
 var ErrUnknownSymbol = errors.New("unknown symbol")
@@ -15,10 +19,14 @@ func NewSymbolTable() *SymbolTable {
 		index: 0,
 		atoi:  make(map[string]int),
 		itoa:  make(map[int]string),
+		mutex: sync.RWMutex{},
 	}
 }
 
 func (st *SymbolTable) Atoi(s string) (int, error) {
+	st.mutex.Lock()
+	defer st.mutex.Unlock()
+
 	if i, ok := st.atoi[s]; ok {
 		return i, nil
 	}
@@ -32,6 +40,9 @@ func (st *SymbolTable) Atoi(s string) (int, error) {
 }
 
 func (st *SymbolTable) Itoa(i int) (string, error) {
+	st.mutex.RLock()
+	defer st.mutex.RUnlock()
+
 	a, ok := st.itoa[i]
 
 	if !ok {
